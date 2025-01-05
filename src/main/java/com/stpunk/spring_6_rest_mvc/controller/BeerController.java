07dd3cce2f3a7e2cd5d1866454cc7d118887bd92
@stpunk47 <stpunk47@gmail.com>
2025-01-05 22:34:04 +0200

    Added simple List implementation for the Request mapping.

--- src/main/java/com/stpunk/spring_6_rest_mvc/controller/BeerController.java
+++ src/main/java/com/stpunk/spring_6_rest_mvc/controller/BeerController.java
@@ -5,15 +5,23 @@ import com.stpunk.spring_6_rest_mvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class BeerController {
 
     private final BeerService beerService;
+
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> listBeers(){
+        return beerService.listBeers();
+    }
 
     public Beer getBeerById(UUID id) {
         log.debug("Getting Beer ID from controller");
--- src/main/java/com/stpunk/spring_6_rest_mvc/services/BeerService.java
+++ src/main/java/com/stpunk/spring_6_rest_mvc/services/BeerService.java
@@ -2,9 +2,12 @@ package com.stpunk.spring_6_rest_mvc.services;
 
 import com.stpunk.spring_6_rest_mvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID id);
 }
