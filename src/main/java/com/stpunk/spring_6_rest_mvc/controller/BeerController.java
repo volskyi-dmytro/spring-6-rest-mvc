34da7bf6a5f6e8cd1177bec22da8c5521ac80218
@stpunk47 <stpunk47@gmail.com>
2025-01-08 00:22:07 +0200

    Added basic POST method

--- src/main/java/com/stpunk/spring_6_rest_mvc/controller/BeerController.java
+++ src/main/java/com/stpunk/spring_6_rest_mvc/controller/BeerController.java
@@ -4,12 +4,13 @@ import com.stpunk.spring_6_rest_mvc.model.Beer;
 import com.stpunk.spring_6_rest_mvc.services.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,6 +21,17 @@ import java.util.UUID;
 public class BeerController {
 
     private final BeerService beerService;
+
+    @PostMapping
+    public ResponseEntity<Beer> handlePost(@RequestBody Beer beer) {
+
+        Beer savedBeer = beerService.saveNewBeer(beer);
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.add("Location", savedBeer.getId().toString());
+        return new ResponseEntity<>(headers, HttpStatus.CREATED);
+    }
 
     @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeers(){
--- src/main/java/com/stpunk/spring_6_rest_mvc/services/BeerServiceImpl.java
+++ src/main/java/com/stpunk/spring_6_rest_mvc/services/BeerServiceImpl.java
@@ -71,4 +71,24 @@ public class BeerServiceImpl implements BeerService {
 
         return beerMap.get(id);
     }
+
+    @Override
+    public Beer saveNewBeer(Beer beer) {
+
+        Beer savedBeer = Beer.builder()
+                .id(UUID.randomUUID())
+                .createdDate(LocalDateTime.now())
+                .updatedDate(LocalDateTime.now())
+                .beerName(beer.getBeerName())
+                .beerStyle(beer.getBeerStyle())
+                .quantityOnHand(beer.getQuantityOnHand())
+                .upc(beer.getUpc())
+                .price(beer.getPrice())
+                .version(beer.getVersion())
+                .build();
+
+        beerMap.put(savedBeer.getId(), savedBeer);
+
+        return savedBeer;
+    }
 }
