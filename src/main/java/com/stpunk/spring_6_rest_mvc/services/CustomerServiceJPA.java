d3f2a457e260baed3b9cede9c7423b911205ed93
@stpunk47 <stpunk47@gmail.com>
2025-01-16 00:15:46 +0200

    Added Integration test for GET operations in Customer Controller.

--- src/main/java/com/stpunk/spring_6_rest_mvc/services/CustomerServiceJPA.java
+++ src/main/java/com/stpunk/spring_6_rest_mvc/services/CustomerServiceJPA.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Primary
@@ -21,12 +22,18 @@ public class CustomerServiceJPA implements CustomerService {
 
     @Override
     public List<CustomerDTO> getCustomers() {
-        return List.of();
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Optional<CustomerDTO> getCustomerById(UUID id) {
-        return Optional.empty();
+        return Optional
+                .ofNullable(customerMapper
+                        .customerToCustomerDTO(customerRepository.findById(id)
+                                .orElse(null)));
     }
 
     @Override
--- src/test/java/com/stpunk/spring_6_rest_mvc/controller/CustomerControllerIT.java
+++ src/test/java/com/stpunk/spring_6_rest_mvc/controller/CustomerControllerIT.java
@@ -1,8 +1,10 @@
 package com.stpunk.spring_6_rest_mvc.controller;
 
 import com.stpunk.spring_6_rest_mvc.entities.Beer;
+import com.stpunk.spring_6_rest_mvc.entities.Customer;
 import com.stpunk.spring_6_rest_mvc.model.BeerDTO;
-import com.stpunk.spring_6_rest_mvc.repositories.BeerRepository;
+import com.stpunk.spring_6_rest_mvc.model.CustomerDTO;
+import com.stpunk.spring_6_rest_mvc.repositories.CustomerRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,49 +18,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class BeerControllerIT {
+class CustomerControllerIT {
 
     @Autowired
-    BeerController beerController;
+    CustomerController customerController;
 
     @Autowired
-    BeerRepository beerRepository;
+    CustomerRepository customerRepository;
 
     @Test
-    void testListBeers() {
-
-        List<BeerDTO> dtos = beerController.listBeers();
+    void testListCustomers() {
+        List<CustomerDTO> dtos = customerController.getCustomers();
 
         assertThat(dtos.size()).isEqualTo(3);
+
     }
 
     @Rollback
     @Transactional
     @Test
     void testEmptyList() {
-        beerRepository.deleteAll();
 
-        List<BeerDTO> dtos = beerController.listBeers();
+        customerRepository.deleteAll();
+
+        List<CustomerDTO> dtos = customerController.getCustomers();
 
         assertThat(dtos.size()).isEqualTo(0);
+
     }
 
     @Test
     void testGetById() {
 
-        Beer beer = beerRepository.findAll().get(0);
+        Customer customer = customerRepository.findAll().get(0);
 
-        BeerDTO dto = beerController.getBeerById(beer.getId());
+        CustomerDTO dto = customerController.getCustomerById(customer.getCustomerId());
 
         assertThat(dto).isNotNull();
     }
 
     @Test
-    void testBeerIdNotFound() {
+    void testCustomerIdNotFound() {
 
         assertThrows(NotFoundException.class, () -> {
-           beerController.getBeerById(UUID.randomUUID());
+            customerController.getCustomerById(UUID.randomUUID());
         });
-
     }
 }
