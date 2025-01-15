955daaba51a77028bc4bc52e9ac7ca6126d028a9
@stpunk47 <stpunk47@gmail.com>
2025-01-15 23:33:20 +0200

    Added basic Integration test for BeerController

--- src/main/java/com/stpunk/spring_6_rest_mvc/services/BeerServiceJPA.java
+++ src/main/java/com/stpunk/spring_6_rest_mvc/services/BeerServiceJPA.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Primary
@@ -21,12 +22,17 @@ public class BeerServiceJPA implements BeerService {
 
     @Override
     public List<BeerDTO> listBeers() {
-        return List.of();
+        return beerRepository.findAll()
+                .stream()
+                .map(beerMapper::beerToBeerDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Optional<BeerDTO> getBeerById(UUID id) {
-        return Optional.empty();
+        return Optional
+                .ofNullable(beerMapper
+                        .beerToBeerDTO(beerRepository.findById(id).orElse(null)));
     }
 
     @Override
---
+++ src/test/java/com/stpunk/spring_6_rest_mvc/controller/BeerControllerIT.java
@@ -0,0 +1,43 @@
+package com.stpunk.spring_6_rest_mvc.controller;
+
+import com.stpunk.spring_6_rest_mvc.model.BeerDTO;
+import com.stpunk.spring_6_rest_mvc.repositories.BeerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerControllerIT {
+
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+
+        List<BeerDTO> dtos = beerController.listBeers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
