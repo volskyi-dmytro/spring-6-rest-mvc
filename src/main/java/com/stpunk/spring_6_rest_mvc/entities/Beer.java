35e89888bacc11a9a5882d4b6dcee975b4202242
@stpunk47 <stpunk47@gmail.com>
2025-01-28 23:38:43 +0200

    Udated Beer entity with Hibernate annotations for timestamps.

--- src/main/java/com/stpunk/spring_6_rest_mvc/entities/Beer.java
+++ src/main/java/com/stpunk/spring_6_rest_mvc/entities/Beer.java
@@ -6,9 +6,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.hibernate.annotations.JdbcType;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.annotations.*;
 import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
@@ -50,7 +48,11 @@ public class Beer {
 
     @NotNull
     private BigDecimal price;
+
+    @CreationTimestamp
     private LocalDateTime createdDate;
+
+    @UpdateTimestamp
     private LocalDateTime updatedDate;
 
 }
