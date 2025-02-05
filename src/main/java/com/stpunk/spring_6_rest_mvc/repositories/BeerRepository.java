c36c66e4bf2a2b258f6d2637cdeb1ae4a09d3bcf
@stpunk47 <stpunk47@gmail.com>
2025-02-06 00:41:20 +0200

    Experiments with the Spring Data REST.

--- src/main/java/com/stpunk/spring_6_rest_mvc/repositories/BeerRepository.java
+++ src/main/java/com/stpunk/spring_6_rest_mvc/repositories/BeerRepository.java
@@ -5,9 +5,11 @@ import com.stpunk.spring_6_rest_mvc.model.BeerStyle;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beer")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
     Page<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName, Pageable pageable);
